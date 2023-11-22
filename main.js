fa1c49c170d130cba9817f718fb2e6e1783cf271
birimbauba <131539137+birimbauba@users.noreply.github.com>
2023-11-22 12:06:34 -0300

    SW

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 script
-function pom (){
-    document.querySelector('#som_tecla_pom').play();
+function Pom (){
+    document.querySelector('#som_tecla_Pom').play();
 }
-document.querySelector('.tecla_pom').onclick = pom;
+document.querySelector('.tecla_Pom').onclick = Pom;
